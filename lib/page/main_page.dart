486108f65f220cc3a0f34302b2d87f602224b961
zhongmeizhi <330447171@qq.com>
2019-07-26 16:51:32 +0800

    开始优化代码

--- lib/page/main_page.dart
+++ lib/page/main_page.dart
@@ -5,12 +5,10 @@ import 'package:zmz_app/page/home/home_page.dart';
 import 'package:zmz_app/page/treasure/treasure_page.dart';
 import 'package:zmz_app/page/customer/customer_page.dart';
 import 'package:zmz_app/page/news/news_page.dart';
-// 请求
-import 'package:zmz_app/api/my_xhr.dart';
-// 更新App操作
-import 'package:zmz_app/unit/update_app.dart';
-// 功能
-import 'package:zmz_app/unit/common/center_nav.dart';
+// 其他
+import 'package:zmz_app/api/my_xhr.dart'; // 请求
+import 'package:zmz_app/unit/update_app.dart'; // 更新App操作
+import 'package:zmz_app/unit/common/center_nav.dart'; // BottomNav加号位置
 
 class MainPage extends StatefulWidget {
   final String title;
@@ -34,18 +32,17 @@ class _MainPageState extends State<MainPage> {
   List _indexedStackCache = <int>[0];
 
   // 初始化 请求的baseURL
-  void setBaseUrl () {
+  void _setBaseUrl () {
     MyXhr myXhr = new MyXhr();
     // myXhr.$option(baseUrl: 'http://10.93.157.7:2333');
-    myXhr.$option(baseUrl: 'https://223b312f.ngrok.io');
+    myXhr.$option(baseUrl: 'https://040c9a18.ngrok.io');
   }
 
   // bottomNavigationBar 点击事件
   void _tapBottomBar (index) {
+    // 解决销毁后setSatte问题
     // Unhandled exception: setState() called after dispose()
-    if (!mounted) {
-      return;
-    }
+    if (!mounted) return;
     setState(() {
       _selectedIndex = index;
       if (_indexedStackCache.indexOf(index) == -1) {
@@ -54,54 +51,20 @@ class _MainPageState extends State<MainPage> {
     });
   }
   
-  void checkAppVersion() {
-    UpdateApp _updateApp = new UpdateApp();
+  void _checkAndUpate() {
     // 可以在第一次打开APP时执行"版本更新"的网络请求
-    _updateApp.getApkLocalInfo().then((directory) {
-      String _version = directory['version'];
-      // 比较服务器的版本号跟当前的版本号，来判断要不要升级APP应用程序
-      if (_updateApp.checkVersionLowerOf(_version)) {
-        String _appPath = directory['url'];
-        // 弹出dalog询问是否升级
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('提示'),
-              content: Text('有优化更新，赶紧体验一下吧。'),
-              actions: [
-                RaisedButton(
-                  textColor: Colors.white,
-                  child: Text('取消'),
-                  onPressed: () {
-                    Navigator.maybePop(context);
-                  },
-                ),
-                RaisedButton(
-                  textColor: Colors.white,
-                  child: Text('确定'),
-                  onPressed: () {
-                    // 下载 并 安装新版本
-                    _updateApp.executeDownload(_appPath);
-                    Navigator.maybePop(context);
-                  },
-                )
-              ]
-            );
-          }
-        );
-      }
-    });
+    UpdateApp _updateApp = new UpdateApp();
+    // context 拿到了耶
+    _updateApp.checkAndUpate(context);
   }
 
   @override
   void initState() {
     super.initState();
     // 设置基础URL信息
-    setBaseUrl();
-    checkAppVersion();
+    _setBaseUrl();
+    _checkAndUpate();
   }
-   static const double offset = 15.0; // offset为正时，表示往下偏移；offset为负时，表示往上偏移
 
   @override
   Widget build(BuildContext context) {
@@ -109,14 +72,14 @@ class _MainPageState extends State<MainPage> {
     return Scaffold(
       key: _scaffoldKey,
       bottomNavigationBar: new BottomNavigationBar( // 底部导航
-        type: BottomNavigationBarType.fixed, // 如果有4个bar那么必须要设置type，可能是BUG
+        type: BottomNavigationBarType.fixed, // 如果有4个bar那么必须要设置type
         backgroundColor: Colors.white,
         items: <BottomNavigationBarItem>[
           new BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('首页')),
-          new BottomNavigationBarItem(icon: Icon(Icons.payment), title: Text('财富')),
-          new BottomNavigationBarItem(icon: Icon(null), title: Text('')), // 空BottomNav
-          new BottomNavigationBarItem(icon: Icon(Icons.book), title: Text('资讯')),
-          new BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('我的')),
+          const BottomNavigationBarItem(icon: Icon(Icons.payment), title: Text('财富')),
+          const BottomNavigationBarItem(icon: Icon(null), title: Text('')), // 空BottomNav
+          const BottomNavigationBarItem(icon: Icon(Icons.book), title: Text('资讯')),
+          const BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('我的')),
         ],
         currentIndex: _selectedIndex,
         fixedColor: Colors.blue,
@@ -124,16 +87,15 @@ class _MainPageState extends State<MainPage> {
       ),
       // IndexedStack 显示第index个child，其它child在页面上是不可见的
       // 但是 每个人page会在IndexedStack初始化的时候都会调用接口
+      // 所以需要添加触发
       body: new IndexedStack(
         index: _selectedIndex,
-        // child: _distributeHome(),
         children: <Widget>[
           new HomePage(),
-          // 暂时先利用 cache 处理 IndexedStack 页面全部初始化问题
-          (_indexedStackCache.indexOf(1) != -1) ? new TreasurePage() : Container(),
+          _cachePage(1, TreasurePage()),
           Container(), // 空BottomNav对应页面
-          (_indexedStackCache.indexOf(3) != -1) ? new NewsPage() : Container(),
-          (_indexedStackCache.indexOf(4) != -1) ? new CustomerPage() : Container(),
+          _cachePage(3, NewsPage()),
+          _cachePage(4, CustomerPage()),
         ],
       ),
       floatingActionButton: Container(
@@ -158,11 +120,17 @@ class _MainPageState extends State<MainPage> {
           backgroundColor: Colors.blue,
           child: Icon(Icons.add, color: Colors.white,),
           onPressed: (){
+            // 黑底布耶
+            // _scaffoldKey.currentState.showBodyScrim(true, 0.5);
           },
         ),
       ),
       // const 很重要，不然每次点击BottomNav就会被重写一次
       floatingActionButtonLocation: const CenterNav()
     );
+  }
+
+  Widget _cachePage(int idx, Widget component) {
+    return (_indexedStackCache.indexOf(idx) != -1) ? component : Container();
   }
 }
--- lib/unit/update_app.dart
+++ lib/unit/update_app.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 // App升级
 import 'package:flutter_downloader/flutter_downloader.dart';
 // 使用 MethodChannel
@@ -6,13 +7,13 @@ import 'package:flutter/services.dart';
 class UpdateApp {
 
     // 检查版本是否低于某版本
-  bool checkVersionLowerOf (String version) {
+  bool _checkVersionLowerOf (String version) {
     // List _versionUtilList = version.split('.');
     return true;
   }
 
   // 获取安装地址
-  Future<Map> getApkLocalInfo() async {
+  Future<Map> _getApkLocalInfo() async {
     // 不发请求了。直接硬编码
     final directory = {
       'version': '1.0.1',
@@ -22,7 +23,7 @@ class UpdateApp {
   }
   
   //下载
-  Future<void> executeDownload(_appPath) async {
+  Future<void> _executeDownload(_appPath) async {
     final taskId = await FlutterDownloader.enqueue(
         url: _appPath + '/app-release.apk',
         savedDir: _appPath,
@@ -31,20 +32,62 @@ class UpdateApp {
     FlutterDownloader.registerCallback((id, status, progress) {
       // 当下载完成时，调用安装
       if (taskId == id && status == DownloadTaskStatus.complete) {
-        installApk(_appPath);
+        _installApk(_appPath);
       }
     });
   }
 
   // 安装
-  Future<Null> installApk(_appPath) async {
+  Future<Null> _installApk(_appPath) async {
     // 本地资源访问
     // zmz_app为项目名
     const platform = const MethodChannel('zmz_app');
     try {
       // 调用app地址
       await platform.invokeMethod('install', {'path': _appPath + '/app-release.apk'});
-    } on PlatformException catch (_) {}
+    } on PlatformException catch (_) {
+      // xxx
+    }
+  }
+
+  // 暴露方法
+  void checkAndUpate(BuildContext context) {
+    // 可以在第一次打开APP时执行"版本更新"的网络请求
+    _getApkLocalInfo().then((directory) {
+      String _version = directory['version'];
+      // 比较服务器的版本号跟当前的版本号，来判断要不要升级APP应用程序
+      if (_checkVersionLowerOf(_version)) {
+        String _appPath = directory['url'];
+        // 弹出dalog询问是否升级
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('提示'),
+              content: Text('有优化更新，赶紧体验一下吧。'),
+              actions: [
+                RaisedButton(
+                  textColor: Colors.white,
+                  child: Text('取消'),
+                  onPressed: () {
+                    Navigator.maybePop(context);
+                  },
+                ),
+                RaisedButton(
+                  textColor: Colors.white,
+                  child: Text('确定'),
+                  onPressed: () {
+                    // 下载 并 安装新版本
+                    _executeDownload(_appPath);
+                    Navigator.maybePop(context);
+                  },
+                )
+              ]
+            );
+          }
+        );
+      }
+    });
   }
 
 }
